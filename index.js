94afd8bc6964cde090bb2b0c659d5f75c26f679e
Recep Öztürk <turkishteacher23@gmail.com>
2020-01-19 09:19:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-function addingEventListener() {
-}
+
+
+
+
+
+
+function addingEventListener(input) {
+  const input = document.getElementById("input");s
+  input.addEventListener("click",function(event)
+  {
+    alert("I am clicked");
+  })
+  }
+addEventListener(input);
